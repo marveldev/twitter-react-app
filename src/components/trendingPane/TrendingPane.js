3688659e2fc14af20123b747ce602e70a5b82dc9
Marvel Dev <marvellousalika48@gmail.com>
2021-02-19 15:14:47 +0100

    add trendModal

--- src/components/trendingPane/TrendingPane.js
+++ src/components/trendingPane/TrendingPane.js
@@ -1,10 +1,31 @@
+import '../trendingPane/trendingPane.css'
+
+const trendModal = `
+  <div class="trend-modal">
+    <button id="closeTrendModalButton">X</button>
+    <strong>Trends</strong>
+    <div class="set-location">
+      <p>Location</p>
+      <span>Show content in this location</span>
+      <input type="checkbox" />
+      <small>When this is on, you'll see what's happening around you right now.</small>
+    </div>
+    <div class="set-trend">
+      <p>Personalization</p>
+      <span>Trends for you</span>
+      <input type="checkbox" />
+      <small>You can personalize trends based on your location and who you follow</small>
+    </div>
+  </div>
+`
+
 const TrendingPane = () => {
   return (
     <div className="trending-pane">
       <i className="fa fa-search"></i>
       <input type="text" className="search-input" placeholder="Search Twitter" />
       <div className="trending-tweets">
-        <strong className="tweets-header">Trends</strong> 
+        <h4>Trends</h4> 
         <button id="trendButton"><i className="material-icons">&#xe8b8;</i></button>
         <div className="current-trend">
           <p>Trending in Nigeria</p>
@@ -49,6 +70,7 @@ const TrendingPane = () => {
           <p>128k Tweets</p>
         </div>
       </div>
+      {trendModal}
     </div>
   )
 }
