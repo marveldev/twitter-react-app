bcb7a8be721383b58af4629c06416c06845cfa8c
Marvel Dev <marvellousalika48@gmail.com>
2021-02-21 18:15:22 +0100

    add openEditModal function

--- src/components/profilePage/ProfilePage.js
+++ src/components/profilePage/ProfilePage.js
@@ -1,6 +1,11 @@
 import './profilePage.css'
 
 const ProfilePage = () => {
+  const openEditModal = () => {
+    document.querySelector('.edit-profile-modal').style.display = 'block'
+    document.querySelector('#overlay').style.display = 'block'
+  }
+
   return (
     <div className="profile-page">
       <div className="header">
@@ -10,7 +15,7 @@ const ProfilePage = () => {
           <small>4 Tweets</small>
         </div>
       </div>
-      <div className="content">
+      <div>
         <img src="https://history.ucr.edu/sites/g/files/rcwecm1916/files/styles/form_preview/public/blank-profile-picture-png.png?itok=MQ-iPuNG"
           className="photo" alt="photo" />
         <div className="user-bio-container">
@@ -30,7 +35,7 @@ const ProfilePage = () => {
               </div>
             </div>
           </div>
-          <button type="button">Edit profile</button>
+          <button type="button" onClick={() => openEditModal()}>Edit profile</button>
         </div>
         <div className="options-button">
           <button>Tweets</button>
@@ -38,6 +43,49 @@ const ProfilePage = () => {
           <button>Media</button>
           <button>Likes</button>
         </div>
+      </div>
+      <div className="edit-profile-modal">
+        <div className="header">
+          <button type="button">X</button>
+          <span>Edit profile</span>
+          <button>Save</button>
+        </div>
+        <div className="content">
+          <div className="user-bio-container">
+            <img src="https://history.ucr.edu/sites/g/files/rcwecm1916/files/styles/form_preview/public/blank-profile-picture-png.png?itok=MQ-iPuNG"
+              className="photo" alt="photo" />
+          </div>
+          <div>
+            <div className="profile-photo">
+              <img src="https://history.ucr.edu/sites/g/files/rcwecm1916/files/styles/form_preview/public/blank-profile-picture-png.png?itok=MQ-iPuNG" alt="photo"/>
+            </div>
+            <div className="entry-box-container">
+              <textarea placeholder="Name"></textarea>
+              <textarea placeholder="Bio"></textarea>
+              <textarea placeholder="Location"></textarea>
+              <textarea placeholder="Website"></textarea>
+              <textarea placeholder="Birth date"></textarea>
+            </div>
+          </div>
+        </div>
+        {/* <div>
+          <strong>Edit Profile</strong>
+          <button id="closeProfileButton">X</button>
+        </div>
+        <div>
+          <div className="profile-entry">
+            <div>
+              <img src='https://history.ucr.edu/sites/g/files/rcwecm1916/files/styles/form_preview/public/blank-profile-picture-png.png?itok=MQ-iPuNG'
+                id="photo" alt="photo" />
+              <input type="file" id="profilePhoto" />
+              <label for="profilePhoto">
+                <strong id="editPhoto">EDIT PHOTO</strong>
+              </label>
+            </div>
+            <input type="text" id="nameInput" placeholder="Enter new name..." required/>
+          </div>
+          <button type="submit" id="saveProfileButton">Save</button>
+        </div> */}
       </div>
     </div>
   )
