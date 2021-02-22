fd12c84cd340be986f775dc10423e097282daa40
Marvel Dev <marvellousalika48@gmail.com>
2021-02-22 17:21:32 +0100

    import BrowerRouter from react-router-dom
    - import Route from react-router-dom
    - import Switch from react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import { TrendingPane, HomePage, LeftNav, ProfilePage } from './components'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import './index.css'
 
 function App() {
   return (
-    <div className="app-layer">
-      <div id="overlay"></div>
-      <LeftNav />
-      <ProfilePage />
-      <TrendingPane />
-    </div>
+    <BrowserRouter>
+      <div className="app-layer">
+        <LeftNav />
+        <div id="overlay"></div>
+        <Switch>
+          <Route path="/" component={HomePage} exact/>
+          <Route path="/profile" component={ProfilePage}/>
+        </Switch>
+        <TrendingPane />
+      </div>
+    </BrowserRouter>
   )
 }
 
