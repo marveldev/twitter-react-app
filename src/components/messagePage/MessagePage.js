c12774a0235ba46fcb0bdc9ecd8aa361087181d6
Marvel Dev <marvellousalika48@gmail.com>
2021-02-24 18:52:54 +0100

    add messagePage JSX div content

--- src/components/messagePage/MessagePage.js
+++ src/components/messagePage/MessagePage.js
@@ -2,11 +2,30 @@ import './messagePage.css'
 
 const MessagePage = () => {
   return (
-    <div>
+    <div className="message-page">
       <div>
         <div className="header">
-          <span>Home</span>
+          <span>Messages</span>
+          <button type="button"><i className="fa fa-envelope-o"></i></button>
         </div>
+        <div className="message-page-content">
+          <div>
+            <i className="fa fa-search"></i>
+            <input type="text" className="search-input" placeholder="Search Twitter" />
+          </div>
+          <div>
+            <p>Send a message, get a message</p>
+            <p>Direct Messages are private conversations between you and other people on Twitter.
+              Share Tweets, media, and more!
+            </p>
+            <button type="button">Start a conversation</button>
+          </div>
+        </div>
+      </div>
+      <div>
+        <p>You don’t have a message selected</p>
+        <p>Choose one from your existing messages, or start a new one.</p>
+        <button type="button">New message</button>
       </div>
     </div>
   )
