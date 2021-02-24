2aa2ceead203c1c428a854fafcdaac914281a0fe
Marvel Dev <marvellousalika48@gmail.com>
2021-02-24 07:23:59 +0100

    add more notification  div items

--- src/components/notificationPage/notificationPage.js
+++ src/components/notificationPage/notificationPage.js
@@ -15,30 +15,64 @@ const NotificationPage = () => {
       </div>
       <div className="notification-page-content">
         <div>
-          <i class="fa fa-bell"></i>
-          <img src="https://history.ucr.edu/sites/g/files/rcwecm1916/files/styles/form_preview/public/blank-profile-picture-png.png?itok=MQ-iPuNG" alt="photo"/>
-          <p>New Tweet notifications for <strong>Ebenezer Don</strong></p>
+          <i class="fa fa-bell symbol"></i>
+          <div>
+            <img src="https://history.ucr.edu/sites/g/files/rcwecm1916/files/styles/form_preview/public/blank-profile-picture-png.png?itok=MQ-iPuNG" alt="photo"/>
+            <p>New Tweet notifications for <strong>Ebenezer Don</strong></p>
+          </div>
         </div>
         <div>
-          <img src="https://history.ucr.edu/sites/g/files/rcwecm1916/files/styles/form_preview/public/blank-profile-picture-png.png?itok=MQ-iPuNG" alt="photo"/>
-          <p><strong>Alo Kayode</strong> followed you</p>
+          <i class="fa fa-user-o symbol"></i>
+          <div>
+            <img src="https://history.ucr.edu/sites/g/files/rcwecm1916/files/styles/form_preview/public/blank-profile-picture-png.png?itok=MQ-iPuNG" alt="photo"/>
+            <p><strong>Alo Kayode</strong> followed you</p>
+          </div>
         </div>
         <div>
-          <i class="fa fa-heart"></i>
-          <img src="https://history.ucr.edu/sites/g/files/rcwecm1916/files/styles/form_preview/public/blank-profile-picture-png.png?itok=MQ-iPuNG" alt="photo"/>
-          <p><strong>Ebenezer Don</strong> liked your Tweet</p>
-          <p>Ten ways to know a junior developer</p>
+          <i class="fa fa-heart heart-icon"></i>
+          <div>
+            <img src="https://history.ucr.edu/sites/g/files/rcwecm1916/files/styles/form_preview/public/blank-profile-picture-png.png?itok=MQ-iPuNG" alt="photo"/>
+            <p><strong>Ebenezer Don</strong> liked your Tweet</p>
+            <p>Ten ways to know a junior developer</p>
+          </div>
         </div>
         <div>
           <i class="fa fa-star"></i>
-          <img src="https://history.ucr.edu/sites/g/files/rcwecm1916/files/styles/form_preview/public/blank-profile-picture-png.png?itok=MQ-iPuNG" alt="photo"/>
-          <p>Recommended For You</p>
-          <p>I'm fired</p>
+          <div>
+            <img src="https://history.ucr.edu/sites/g/files/rcwecm1916/files/styles/form_preview/public/blank-profile-picture-png.png?itok=MQ-iPuNG" alt="photo"/>
+            <p>Recommended For You</p>
+            <p>I'm fired</p>
+          </div>
         </div>
         <div>
-          <img src="https://history.ucr.edu/sites/g/files/rcwecm1916/files/styles/form_preview/public/blank-profile-picture-png.png?itok=MQ-iPuNG" alt="photo"/>
-          <p><strong>John Ademoye</strong> liked tour Tweet</p>
-          <p>hey</p>
+          <i class="fa fa-heart heart-icon"></i>
+          <div>
+            <img src="https://history.ucr.edu/sites/g/files/rcwecm1916/files/styles/form_preview/public/blank-profile-picture-png.png?itok=MQ-iPuNG" alt="photo"/>
+            <p><strong>John Ademoye</strong> liked tour Tweet</p>
+            <p>hey</p>
+          </div>
+        </div>
+        <div>
+          <i class="fa fa-bell symbol"></i>
+          <div>
+            <img src="https://history.ucr.edu/sites/g/files/rcwecm1916/files/styles/form_preview/public/blank-profile-picture-png.png?itok=MQ-iPuNG" alt="photo"/>
+            <p>New Tweet notifications for <strong>Ebenezer Don</strong></p>
+          </div>
+        </div>
+        <div>
+          <i class="fa fa-user-o symbol"></i>
+          <div>
+            <img src="https://history.ucr.edu/sites/g/files/rcwecm1916/files/styles/form_preview/public/blank-profile-picture-png.png?itok=MQ-iPuNG" alt="photo"/>
+            <p><strong>Alo Kayode</strong> followed you</p>
+          </div>
+        </div>
+        <div>
+          <i class="fa fa-star"></i>
+          <div>
+            <img src="https://history.ucr.edu/sites/g/files/rcwecm1916/files/styles/form_preview/public/blank-profile-picture-png.png?itok=MQ-iPuNG" alt="photo"/>
+            <p>Recommended For You</p>
+            <p>I'm fired</p>
+          </div>
         </div>
       </div>
     </div>
