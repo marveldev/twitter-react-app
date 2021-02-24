860e8480eeab443e2facf140608ccadae14aca42
Marvel Dev <marvellousalika48@gmail.com>
2021-02-24 08:29:28 +0100

    add bookmarkPage JSX div content

--- src/components/bookmarkPage/BookmarkPage.js
+++ src/components/bookmarkPage/BookmarkPage.js
@@ -1,6 +1,16 @@
+import './bookmarkPage.css'
+
 const BookmarkPage = () => {
   return (
-    <div>Hello</div>
+    <div className="bookmarks-page">
+      <div className="header">
+        <span>Bookmarks</span>
+      </div>
+      <div className="bookmarks-page-content">
+        <p>You haven't added any Tweets to your Bookmarks yet</p>
+        <span>When you do, they 'll show up here.</span>
+      </div>
+    </div>
   )
 }
 
