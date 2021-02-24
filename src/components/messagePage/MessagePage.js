ab831c64e75dd4718c0eec446ebcacb093c61b29
Marvel Dev <marvellousalika48@gmail.com>
2021-02-24 18:35:16 +0100

    initial commit

---
+++ src/components/messagePage/MessagePage.js
@@ -0,0 +1,15 @@
+import './messagePage.css'
+
+const MessagePage = () => {
+  return (
+    <div>
+      <div>
+        <div className="header">
+          <span>Home</span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default MessagePage
