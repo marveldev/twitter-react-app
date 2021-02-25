b6d0f7918f59e1aa2cecbf0f0b3ff95c47cd1229
Marvel Dev <marvellousalika48@gmail.com>
2021-02-25 12:36:11 +0100

    modify message-page-content

--- src/components/messagePage/MessagePage.js
+++ src/components/messagePage/MessagePage.js
@@ -4,28 +4,30 @@ const MessagePage = () => {
   return (
     <div className="message-page">
       <div>
-        <div className="header">
+        <div className="message-header">
           <span>Messages</span>
           <button type="button"><i className="fa fa-envelope-o"></i></button>
         </div>
         <div className="message-page-content">
           <div>
-            <i className="fa fa-search"></i>
-            <input type="text" className="search-input" placeholder="Search Twitter" />
-          </div>
-          <div>
-            <p>Send a message, get a message</p>
-            <p>Direct Messages are private conversations between you and other people on Twitter.
-              Share Tweets, media, and more!
-            </p>
-            <button type="button">Start a conversation</button>
+            <div id="searchInputContainer">
+              <i className="fa fa-search"></i>
+              <input type="text" className="search-input" placeholder="Search Twitter" />
+            </div>
+            <div id="messageInfo">
+              <strong>Send a message, get a message</strong>
+              <p>Direct Messages are private conversations between you and other people on Twitter.
+                Share Tweets, media, and more!
+              </p>
+              <button type="button" className="message-button">Start a conversation</button>
+            </div>
           </div>
         </div>
       </div>
-      <div>
-        <p>You don’t have a message selected</p>
+      <div className="message-pane">
+        <strong>You don’t have a message selected</strong>
         <p>Choose one from your existing messages, or start a new one.</p>
-        <button type="button">New message</button>
+        <button type="button" className="message-button">New message</button>
       </div>
     </div>
   )
