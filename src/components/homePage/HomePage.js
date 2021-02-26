1e4214d6f3fc2bba91212b8aca6af829636aa07c
Marvel Dev <marvellousalika48@gmail.com>
2021-02-26 10:33:00 +0100

    add responsiveness for screen size with max-width of 768px

--- src/components/homePage/HomePage.js
+++ src/components/homePage/HomePage.js
@@ -6,30 +6,32 @@ const HomePage = () => {
       <div className="header">
         <span>Home</span>
       </div>
-      <div id="tweetContainer">
-        <img src='https://history.ucr.edu/sites/g/files/rcwecm1916/files/styles/form_preview/public/blank-profile-picture-png.png?itok=MQ-iPuNG'
-          className="home-page-photo" alt="photo" />
-        <div>
+      <div className="home-page-content">
+        <div id="tweetContainer">
+          <img src='https://history.ucr.edu/sites/g/files/rcwecm1916/files/styles/form_preview/public/blank-profile-picture-png.png?itok=MQ-iPuNG'
+            className="home-page-photo" alt="photo" />
           <div>
-            <textarea className="home-tweet-input" placeholder="What's happening?"></textarea>
-            <strong>Everyone can reply</strong>
-          </div>
-          <div className="tweet-options">
             <div>
-              <input type="file" id="addPhoto" />
-              <label for="addPhoto">
-                <span><i className="fa fa-file-picture-o" id="photoIcon"></i></span>
-              </label>
-              <span><i className="fa fa-git-square"></i></span>
-              <span><i className="fa fa-bar-chart"></i></span>
-              <span><i className="fa fa-smile-o"></i></span>
-              <span><i className="fa fa-calendar-plus-o"></i></span>
+              <textarea className="home-tweet-input" placeholder="What's happening?"></textarea>
+              <strong>Everyone can reply</strong>
             </div>
-            <button type="button" className="add-tweet-button">Tweet</button>
+            <div className="tweet-options">
+              <div>
+                <input type="file" id="addPhoto" />
+                <label for="addPhoto">
+                  <span><i className="fa fa-file-picture-o" id="photoIcon"></i></span>
+                </label>
+                <span><i className="fa fa-git-square"></i></span>
+                <span><i className="fa fa-bar-chart"></i></span>
+                <span><i className="fa fa-smile-o"></i></span>
+                <span><i className="fa fa-calendar-plus-o"></i></span>
+              </div>
+              <button type="button" className="add-tweet-button">Tweet</button>
+            </div>
           </div>
         </div>
+        <div id="tweetOutput"></div>
       </div>
-      <div id="tweetOutput"></div>
     </div>
   )
 }
--- src/components/leftNav/LeftNav.js
+++ src/components/leftNav/LeftNav.js
@@ -12,34 +12,43 @@ const LeftNav = () => {
       <div className="left-nav-content">
         <span id="logo"><i className="fa">&#xf099;</i></span>
         <Link to="/" className="nav-button home active" onClick={() => addActiveClass('.home')}> 
-          <i className="glyphicon glyphicon-home"></i>Home
+          <i className="glyphicon glyphicon-home"></i>
+          <span>Home</span>
         </Link>
         <Link to="/explore" className="nav-button explore"
           onClick={() => addActiveClass('.explore')}>
-          <i className="fa fa-hashtag"></i>Explore
+          <i className="fa fa-hashtag"></i>
+          <span>Explore</span>
         </Link>
         <Link to="/notification" className="nav-button notification"
           onClick={() => addActiveClass('.notification')}>
-          <i className="fa fa-bell-o"></i>Notifications
+          <i className="fa fa-bell-o"></i>
+          <span>Notifications</span>
         </Link>
         <Link to="/messages" className="nav-button messages"
           onClick={() => addActiveClass('.messages')}>
-          <i className="fa fa-envelope-o"></i>Messages
+          <i className="fa fa-envelope-o"></i>
+          <span>Messages</span>
         </Link>
-        <Link to="/bookmark" className="nav-button bookmarks"
+        <Link to="/bookmark" className="bookmarks nav-button"
           onClick={() => addActiveClass('.bookmarks')}>
-          <i className="fa fa-bookmark-o"></i>Bookmarks
+          <i className="fa fa-bookmark-o"></i>
+          <span>Bookmarks</span>
         </Link>
-        <Link to="/list" className="nav-button lists" onClick={() => addActiveClass('.lists')}>
-          <i className="fa fa-list-alt"></i>Lists
+        <Link to="/list" className="lists nav-button" onClick={() => addActiveClass('.lists')}>
+          <i className="fa fa-list-alt"></i>
+          <span>Lists</span>
         </Link>
         <Link to="/profile" className="nav-button profile"
           onClick={() => addActiveClass('.profile')}>
-          <i className="fa fa-user-o"></i>Profile
+          <i className="fa fa-user-o"></i>
+          <span>Profile</span>
         </Link>
-        <button type="button" className="nav-button">
-          <i className="fa fa-caret-down"></i>More
+        <button type="button" className="more nav-button">
+          <i className="fa fa-caret-down"></i>
+          <span>More</span>
         </button>
+        <button className="tweet nav-button"><i class="material-icons">&#xe0cb;</i></button>
         <button type="button" id="tweetModalButton">Tweet</button>
         <div className="user-info">
           <img src='https://history.ucr.edu/sites/g/files/rcwecm1916/files/styles/form_preview/public/blank-profile-picture-png.png?itok=MQ-iPuNG'
