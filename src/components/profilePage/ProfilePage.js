83163a24fc857b0410841a16f8e4c7527783dbc3
Marvel Dev <marvellousalika48@gmail.com>
2021-02-26 07:54:06 +0100

    remove Bio function for props and state practise code

--- src/components/profilePage/ProfilePage.js
+++ src/components/profilePage/ProfilePage.js
@@ -1,53 +1,4 @@
-import { useState } from 'react'
 import './profilePage.css'
-
-const Bio = (props) => {
-  const { dateOfBirth } = props
-  const [name, setName] = useState('Jane Doe')
-  const [location, setLocation] = useState({
-    city: 'Lagos',
-    country: 'Nigeria'
-  })
-
-  const updateName = (event) => {
-    event.preventDefault()
-    const nameInput = document.querySelector('.name-field').value
-    setName(nameInput)
-  }
-
-  const updateLocation = (event) => {
-    event.preventDefault()
-    const cityInput = document.querySelector('.city-field').value
-    const countryInput = document.querySelector('.country-field').value
-    setLocation({
-      city: cityInput,
-      country: countryInput
-    })
-  }
-
-  return (
-    <div className="user-bio">
-      <strong>{name}</strong>
-      <p>Front-end developer || Christian</p>
-      <span><i className="material-icons">&#xe55f;</i>{location.city}, {location.country}</span>
-      <span><i className="fa fa-birthday-cake"></i>Born {dateOfBirth}</span>
-      <span><i className="fa fa-calendar"></i>Joined January 2021</span>
-      <div>
-        <span><strong>48 </strong>Following</span>
-        <span><strong>8 </strong>Followers</span>
-      </div>
-      <form onSubmit={(event) => updateName(event)}>
-        <input type="text" className="name-field" />
-        <button type="submit">Change name</button>
-      </form>
-      <form onSubmit={(event) => updateLocation(event)}>
-        <input type="text" className="city-field" />
-        <input type="text" className="country-field" />
-        <button type="submit">Change location</button>
-      </form>
-    </div>
-  )
-}
 
 const ProfilePage = () => {
   const openEditModal = () => {
@@ -72,11 +23,17 @@ const ProfilePage = () => {
             <div className="profile-photo">
               <img src="https://history.ucr.edu/sites/g/files/rcwecm1916/files/styles/form_preview/public/blank-profile-picture-png.png?itok=MQ-iPuNG" alt="photo"/>
             </div>
-            <Bio
-              name="Judith Beans"
-              dateOfBirth="$th April 1865"
-              city="Lagos, Nigeria"
-            />
+            <div className="user-bio">
+              <strong>Jane Doe</strong>
+              <p>Front-end developer || Christian</p>
+              <span><i className="material-icons">&#xe55f;</i>Lagos, Nigeria</span>
+              <span><i className="fa fa-birthday-cake"></i>Born April 5th 1999</span>
+              <span><i className="fa fa-calendar"></i>Joined January 2021</span>
+              <div>
+                <span><strong>48 </strong>Following</span>
+                <span><strong>8 </strong>Followers</span>
+              </div>
+            </div>
           </div>
           <button type="button" onClick={() => openEditModal()}>Edit profile</button>
         </div>
