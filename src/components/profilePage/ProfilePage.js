ab38af8da78da0de7e975a4e0e6a74f375d9bdc8
Marvel Dev <marvellousalika48@gmail.com>
2021-03-01 16:39:20 +0100

    add state for form inputs

--- src/components/profilePage/ProfilePage.js
+++ src/components/profilePage/ProfilePage.js
@@ -7,6 +7,11 @@ const ProfilePage = () => {
   const [profilePhoto, setProfilePhoto] = useState(defaultPhotoSource)
   const [modalDisplay, setModalDisplay] = useState(false)
   const [deleteButton, setDeleteButton] = useState('none')
+  const [name, setName] = useState('Jane Doe')
+  const [bio, setBio] = useState('Bio')
+  const [location, setLocation] = useState('Location')
+  const [website, setWebsite] = useState('Website')
+  const [birthDate, setBirthDate] = useState()
 
   const addPhoto = (id) => {
     const photoReader = new FileReader()
@@ -21,6 +26,21 @@ const ProfilePage = () => {
     })
   }
 
+  const updateBio = () => {
+    const nameInput = document.querySelector('.name-field').value
+    const bioInput = document.querySelector('.bio-field').value
+    const locationInput = document.querySelector('.location-field').value
+    const websiteInput = document.querySelector('.website-field').value
+    const birthDateInput = document.querySelector('.birth-date-field').value
+    console.log(birthDateInput);
+    setName(nameInput)
+    setBio(bioInput)
+    setLocation(locationInput)
+    setWebsite(websiteInput)
+    setBirthDate(birthDateInput)
+    setModalDisplay(false)
+  }
+
   const editProfileModal = (
     <>
       <div className="overlay" onClick={() => setModalDisplay(false)}></div>
@@ -30,7 +50,7 @@ const ProfilePage = () => {
             onClick={() => setModalDisplay(false)}>X
           </button>
           <span>Edit profile</span>
-          <button id="saveProfileButton">Save</button>
+          <button id="saveProfileButton" onClick={() => updateBio()}>Save</button>
         </div>
         <div className="content">
           <img src={topPhoto} className="photo" alt="photo" />
@@ -59,12 +79,16 @@ const ProfilePage = () => {
               </label>
             </div>
             <div className="entry-box-container">
-              <input type="text" maxLength="20" placeholder="Name" required/>
-              <textarea placeholder="Bio"></textarea>
-              <input type="text" maxLength="20" placeholder="Location"/>
-              <textarea placeholder="Website"></textarea>
+              <input type="text" className="name-field"
+                maxLength="20" placeholder={name} required
+              />
+              <textarea className="bio-field" placeholder={bio}></textarea>
+              <input type="text" className="location-field"
+                maxLength="20" placeholder={location}
+              />
+              <textarea className="website-field" placeholder={website}></textarea>
               <label>Birth date
-                <input type="date" />
+                <input type="date" className="birth-date-field" placeholder={birthDate} />
               </label>
             </div>
           </div>
@@ -78,7 +102,7 @@ const ProfilePage = () => {
       <div className="header">
         <button type="button"><i className="material-icons">&#xe5c4;</i></button>
         <div>
-          <span>Jane Doe</span>
+          <span>{name}</span>
           <small>4 Tweets</small>
         </div>
       </div>
@@ -90,10 +114,10 @@ const ProfilePage = () => {
               <img src={profilePhoto} alt="photo"/>
             </div>
             <div className="user-bio">
-              <strong>Jane Doe</strong>
-              <p>Front-end developer || Christian</p>
-              <span><i className="material-icons">&#xe55f;</i>Lagos, Nigeria</span>
-              <span><i className="fa fa-birthday-cake"></i>Born April 5th 1999</span>
+              <strong>{name}</strong>
+              <p>{bio}</p>
+              <span><i className="material-icons">&#xe55f;</i>{location}</span>
+              <span><i className="fa fa-birthday-cake"></i>Born {birthDate}</span>
               <span><i className="fa fa-calendar"></i>Joined January 2021</span>
               <div>
                 <span><strong>48 </strong>Following</span>
