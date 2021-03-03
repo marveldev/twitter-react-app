0e391e3e4822b496f7723ee3297e208bad5b4ecf
Marvel Dev <marvellousalika48@gmail.com>
2021-03-03 18:30:26 +0100

    add commentModal state
    - add CommentModal as a component
    - add Comments as a component

--- src/components/commentPage/CommentPage.js
+++ src/components/commentPage/CommentPage.js
@@ -1,15 +1,21 @@
+import { useState } from 'react'
+import CommentModal from './CommentModal'
+import Comments from "./Comments";
+import './commentPage.css'
+
 const CommentPage = () => {
+  const [commentModalDisplay, setCommentModal] = useState(false)
   return (
     <div className="comment-page">
-      <div className="home">
-        <button id="returnButton"><i className="material-icons">&#xe5c4;</i></button>
+      <div className="header">
+        <button><i className="material-icons">&#xe5c4;</i></button>
         <span>Tweet</span>
       </div>
       <div className="tweet-content">
         <div>
-          <img src="" className="home-page-photo image" alt="user-profile" />
+          <img src="https://history.ucr.edu/sites/g/files/rcwecm1916/files/styles/form_preview/public/blank-profile-picture-png.png?itok=MQ-iPuNG" className="home-page-photo" alt="user-profile" />
           <strong className="user-profile-name">Jane Doe</strong>
-          <button className="more-button"><i className="material-icons">&#xe5d3;</i></button>
+          <button><i className="material-icons">&#xe5d3;</i></button>
         </div>
         <div>
           <p className="tweet-text">Hey All</p>
@@ -21,37 +27,20 @@ const CommentPage = () => {
           <span><strong>675 </strong>Likes</span>
         </div>
         <div className="user-tweet-options">
-          <button className="comment-modal-button"><i className="fa fa-comment-o"></i></button>
+          <button
+            title="Reply"
+            onClick={() => setCommentModal(true)}
+          >
+            <i className="fa fa-comment-o"></i>
+          </button>
           <button><i className="fa fa-retweet"></i></button>
           <button><i className="fa fa-heart-o"></i></button>
           <button><i className="fa fa-upload"></i></button>
         </div>
-        <div id="commentOutput"></div>
       </div>
-      <div className="comment-modal-container">
-        <div>
-          <button className="close-comment-button">X</button>
-        </div>
-        <div className="about-comment">
-          <img src="" className="home-page-photo image" alt="user-profile" />
-          <div>
-            <span className="user-profile-name">Jane Doe</span>
-            <p>Hey All</p>
-          </div>
-        </div>
-        <div className="tweet-input">
-          <img src="" className="home-page-photo" alt="user-profile" />
-          <textarea className="comment-box" placeholder="Tweet your reply"></textarea>
-        </div>
-        <div className="tweet-options">
-          <span><i className="fa fa-file-picture-o" id="photoIcon"></i></span>
-          <span><i className="material-icons">&#xe908</i></span>
-          <span><i className="fa fa-bar-chart"></i></span>
-          <span><i className="fa fa-smile-o"></i></span>
-          <span><i className="fa fa-calendar-plus-o"></i></span>
-          <button className="add-comment-button">Reply</button>
-        </div>
-      </div>
+      <div id="commentOutput"></div>
+      {commentModalDisplay && <CommentModal setCommentModal={setCommentModal}/>}
+      {<Comments />}
     </div>
   )
 }
