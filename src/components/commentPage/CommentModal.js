ac31126126d1ac5d29afc56917450296a2f70823
Marvel Dev <marvellousalika48@gmail.com>
2021-03-03 18:29:15 +0100

    initial commit

---
+++ src/components/commentPage/CommentModal.js
@@ -0,0 +1,35 @@
+const CommentModal = ({ setCommentModal }) => {
+  return (
+    <>
+      <div className="overlay" onClick={() => setCommentModal(false)}></div>
+      <div className="comment-modal">
+        <div className="tweet-modal-header">
+          <button type="button" onClick={() => setCommentModal(false)}>X</button>
+        </div>
+        <div className="comment-content">
+          <img src="https://history.ucr.edu/sites/g/files/rcwecm1916/files/styles/form_preview/public/blank-profile-picture-png.png?itok=MQ-iPuNG" className="home-page-photo" alt="user-profile" />
+          <div>
+            <span className="user-profile-name">Jane Doe</span>
+            <p>Hey All</p>
+          </div>
+        </div>
+        <div className="comment-input">
+          <img src="https://history.ucr.edu/sites/g/files/rcwecm1916/files/styles/form_preview/public/blank-profile-picture-png.png?itok=MQ-iPuNG" className="home-page-photo" alt="user-profile" />
+          <textarea className="home-tweet-input" placeholder="Tweet your reply"></textarea>
+        </div>
+        <div className="tweet-options">
+          <div>
+            <span><i className="fa fa-file-picture-o"></i></span>
+            <span><i className="fa fa-git-square"></i></span>
+            <span><i className="fa fa-bar-chart"></i></span>
+            <span><i className="fa fa-smile-o"></i></span>
+            <span><i className="fa fa-calendar-plus-o"></i></span>
+          </div>
+          <button className="add-tweet-button">Reply</button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default CommentModal
---
+++ src/components/commentPage/Comments.js
@@ -0,0 +1,24 @@
+const Comments = () => {
+  return (
+    <div class="comment-item">
+      <div className="tweet-content-item">
+        <img src="https://history.ucr.edu/sites/g/files/rcwecm1916/files/styles/form_preview/public/blank-profile-picture-png.png?itok=MQ-iPuNG" className="home-page-photo" alt="user-profile" />
+        <div>
+          <div className="tweet-person">
+            <strong>Derick Alangi</strong>
+            <button><i className="material-icons">&#xe5d3;</i></button>
+          </div>
+          <p>Hey you, Howdy?</p>
+          <div className="tweet-info">
+            <button><i className="fa fa-comment-o"></i>5.1k</button>
+            <button><i className="fa fa-retweet"></i>2.1k</button>
+            <button><i className="fa fa-heart-o"></i>3.1k</button>
+            <button><i className="fa fa-upload"></i></button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Comments
