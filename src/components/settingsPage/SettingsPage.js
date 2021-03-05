eb4a019025b23419af9a59c57060a6b3fc09eee0
Marvel Dev <marvellousalika48@gmail.com>
2021-03-05 15:36:29 +0100

    add currentSection state

--- src/components/settingsPage/SettingsPage.js
+++ src/components/settingsPage/SettingsPage.js
@@ -1,37 +1,61 @@
+import { useState } from 'react'
+import AccessiblitySection from './AccessiblitySection.js'
+import AccountSection from './AccountSection'
+import NotificationSection from './NotificationSection.js'
+import SecuritySection from './SecuritySection'
 import './settingsPage.css'
 
 const SettingsPage = () => {
+  const [currentSection, setCurrentSection] = useState('account')
   return (
     <div className="settings-page">
-      <div className="headline">
-        <span>Settings</span>
+      <div>
+        <div className="headline">
+          <span>Settings</span>
+        </div>
+        <div className="settings page-core">
+          <button
+            onClick={() => setCurrentSection('account')}
+            type="button" className={currentSection === 'account' ? 'current-section' : ''}
+          >
+            <span>Your account</span>
+            <i class="material-icons">&#xe315;</i>
+          </button>
+          <button
+            onClick={() => setCurrentSection('security')}
+            type="button" className={currentSection === 'security' ? 'current-section' : ''}
+          >
+            <span>Security and account access</span>
+            <i class="material-icons">&#xe315;</i>
+          </button>
+          <button type="button">
+            <span>Privacy and safety</span>
+            <i class="material-icons">&#xe315;</i>
+          </button>
+          <button
+            onClick={() => setCurrentSection('notification')}
+            type="button" className={currentSection === 'notification' ? 'current-section' : ''}
+          >
+            <span>Notifications</span>
+            <i class="material-icons">&#xe315;</i>
+          </button>
+          <button
+            onClick={() => setCurrentSection('accessibility')}
+            type="button" className={currentSection === 'accessibility' ? 'current-section' : ''}
+          >
+            <span>Accessibility, display, and languages</span>
+            <i class="material-icons">&#xe315;</i>
+          </button>
+          <button type="button">
+            <span>Additional resources</span>
+            <i class="material-icons">&#xe315;</i>
+          </button>
+        </div>
       </div>
-      <div className="settings page-core">
-        <button type="button" className="current-section">
-          <span>Your account</span>
-          <i class="material-icons">&#xe315;</i>
-        </button>
-        <button type="button">
-          <span>Security and account access</span>
-          <i class="material-icons">&#xe315;</i>
-        </button>
-        <button type="button">
-          <span>Privacy and safety</span>
-          <i class="material-icons">&#xe315;</i>
-        </button>
-        <button type="button">
-          <span>Notifications</span>
-          <i class="material-icons">&#xe315;</i>
-        </button>
-        <button type="button">
-          <span>Accessibility, display, and languages</span>
-          <i class="material-icons">&#xe315;</i>
-        </button>
-        <button type="button">
-          <span>Additional resources</span>
-          <i class="material-icons">&#xe315;</i>
-        </button>
-      </div>
+      {currentSection === 'account' && <AccountSection />}
+      {currentSection === 'security' && <SecuritySection />}
+      {currentSection === 'notification' && <NotificationSection />}
+      {currentSection === 'accessibility' && <AccessiblitySection />}
     </div>
   )
 }
