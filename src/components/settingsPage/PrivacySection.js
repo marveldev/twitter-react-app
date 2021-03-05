e835e5fa15498349c0f7ec061acdbdfed5b9210a
Marvel Dev <marvellousalika48@gmail.com>
2021-03-05 16:19:23 +0100

    initial commit

---
+++ src/components/settingsPage/PrivacySection.js
@@ -0,0 +1,125 @@
+const PrivacySection = () => {
+  return (
+    <div className="privacy-section">
+      <div className="section header">
+        <span>Privacy and safety</span>
+      </div>
+      <div className="section-content">
+        <div>
+          <span>Manage what information you see and share on Twitter.</span>
+        </div>
+        <div>
+          <h3>Your Twitter activity</h3>
+        </div>
+        <section>
+          <button type="button" className="button">
+            <p className="material-icons">&#xe152;</p>
+            <div>
+              <p>Audience and tagging</p>
+              <span>Manage what information you allow other people on Twitter to see.</span>
+            </div>
+            <i className="material-icons">&#xe315;</i>
+          </button>
+          <button type="button" className="button">
+            <p className="material-icons">&#xe152;</p>
+            <div>
+              <p>Your Tweets</p>
+              <span>Manage the information associated with your Tweets. </span>
+            </div>
+            <i className="material-icons">&#xe315;</i>
+          </button>
+          <button type="button" className="button">
+            <p className="material-icons">&#xe152;</p>
+            <div>
+              <p>Content you see</p>
+              <span>Decide what you see on Twitter based on
+                your preferences like Topics and interests
+              </span>
+            </div>
+            <i className="material-icons">&#xe315;</i>
+          </button>
+          <button type="button" className="button">
+            <p className="material-icons">&#xe152;</p>
+            <div>
+              <p>Filters</p>
+              <span>Choose the notification you'd like to see — and those you don't.</span>
+            </div>
+            <i className="material-icons">&#xe315;</i>
+          </button>
+          <button type="button" className="button">
+            <p className="material-icons">&#xe152;</p>
+            <div>
+              <p>Filters</p>
+              <span>Choose the notification you'd like to see — and those you don't.</span>
+            </div>
+            <i className="material-icons">&#xe315;</i>
+          </button>
+          <button type="button" className="button">
+            <p className="material-icons">&#xe152;</p>
+            <div>
+              <p>Filters</p>
+              <span>Choose the notification you'd like to see — and those you don't.</span>
+            </div>
+            <i className="material-icons">&#xe315;</i>
+          </button>
+        </section>
+        <div>
+          <h3>Data sharing and off-Twitter activity</h3>
+        </div>
+        <section>
+          <button type="button" className="button">
+            <p className="material-icons">&#xe152;</p>
+            <div>
+              <p>Filters</p>
+              <span>Choose the notification you'd like to see — and those you don't.</span>
+            </div>
+            <i className="material-icons">&#xe315;</i>
+          </button>
+          <button type="button" className="button">
+            <p className="material-icons">&#xe152;</p>
+            <div>
+              <p>Filters</p>
+              <span>Choose the notification you'd like to see — and those you don't.</span>
+            </div>
+            <i className="material-icons">&#xe315;</i>
+          </button>
+          <button type="button" className="button">
+            <p className="material-icons">&#xe152;</p>
+            <div>
+              <p>Filters</p>
+              <span>Choose the notification you'd like to see — and those you don't.</span>
+            </div>
+            <i className="material-icons">&#xe315;</i>
+          </button>
+          <button type="button" className="button">
+            <p className="material-icons">&#xe152;</p>
+            <div>
+              <p>Filters</p>
+              <span>Choose the notification you'd like to see — and those you don't.</span>
+            </div>
+            <i className="material-icons">&#xe315;</i>
+          </button>
+        </section>
+        <div>
+          <h3>Learn more about privacy on Twitter</h3>
+        </div>
+        <section className="more-privacy-options">
+          <button type="button" className="button">
+            <p>Privacy center</p>
+            <i className="material-icons">&#xe315;</i>
+          </button>
+          <button type="button" className="button">
+            <p>Privacy policy</p>
+            <i className="material-icons">&#xe315;</i>
+          </button>
+          <button type="button" className="button">
+            <p>Contact us</p>
+            <i className="material-icons">&#xe315;</i>
+          </button>
+        </section>
+      </div>
+    </div>
+  )
+}
+
+export default PrivacySection
