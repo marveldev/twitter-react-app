9d9433131ca61a75c219e266257863390669a690
Marvel Dev <marvellousalika48@gmail.com>
2021-03-06 08:56:32 +0100

    initial commit

---
+++ src/components/settingsPage/DisplaySection.js
@@ -0,0 +1,11 @@
+const DisplaySection = () => {
+  return (
+    <div>
+      <div className="section header">
+        <span>Display</span>
+      </div>
+    </div>
+  )
+}
+
+export default DisplaySection
