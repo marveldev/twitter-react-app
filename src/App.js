ddd1fd0c4c258df67892b27e18a5d6bf1221f0dc
Marvel Dev <marvellousalika48@gmail.com>
2021-03-11 14:12:00 +0100

    add theme as parameter to leftNav

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <div className={`app-layer ${theme}`}>
-        <LeftNav setTheme={setTheme}/>
+        <LeftNav theme={theme} setTheme={setTheme}/>
         <Switch>
           <Route path="/" component={HomePage} exact/>
           <Route path="/comment" component={CommentPage}/>
@@ -29,6 +29,7 @@ const App = () => {
             path="/settings"
             component={() => (
               <SettingsPage
+                theme={theme}
                 setTheme={setTheme}
               />
             )}
