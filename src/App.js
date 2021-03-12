d22ad439b9b1a6cb2b50aba67c1a3b708dd7d0af
Marvel Dev <marvellousalika48@gmail.com>
2021-03-12 15:47:49 +0100

    add tweetaData and commentModal state

--- src/App.js
+++ src/App.js
@@ -9,15 +9,34 @@ import { useState } from 'react'
 const App = () => {
   const storedTheme = localStorage.getItem('storedTheme')
   const [theme, setTheme] = useState(storedTheme || '')
+  const [tweetData, setTweetData] = useState([])
+  const [commentModal, setCommentModal] = useState(false)
 
   return (
     <BrowserRouter>
       <div className={`app-layer ${theme}`}>
         <LeftNav theme={theme} setTheme={setTheme}/>
         <Switch>
-          <Route path="/" component={HomePage} exact/>
+          <Route path="/"
+            component={() => (
+              <HomePage
+                tweetData={tweetData}
+                setTweetData={setTweetData}
+                commentModal={commentModal}
+                setCommentModal={setCommentModal}
+              />
+            )} exact
+          />
           <Route path="/comment" component={CommentPage}/>
-          <Route path="/profile" component={ProfilePage}/>
+          <Route path="/profile"
+            component={() => (
+              <ProfilePage
+                tweetData={tweetData}
+                commentModal={commentModal}
+                setCommentModal={setCommentModal}
+              />
+            )}
+          />
           <Route path="/explore" component={ExplorePage}/>
           <Route path="/notification" component={NotificationPage}/>
           <Route path="/bookmark" component={BookmarkPage}/>
