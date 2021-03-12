b19161ce5fcf77c2219d6b957f30e2b38a3cc0c7
Marvel Dev <marvellousalika48@gmail.com>
2021-03-12 11:30:45 +0100

    return tweets items from tweetData

--- src/components/homePage/Tweets.js
+++ src/components/homePage/Tweets.js
@@ -2,34 +2,34 @@ import { useState } from "react"
 import { Link } from "react-router-dom"
 import CommentModal from "../commentPage/CommentModal"
 
-const Tweets = ({ tweetText }) => {
+const Tweets = ({ tweetData }) => {
   const [commentModalDisplay, setCommentModal] = useState(false)
 
   return (
-    <div className="tweet-item">
-      <div className="tweet-content-item">
-        <img src="https://history.ucr.edu/sites/g/files/rcwecm1916/files/styles/form_preview/public/blank-profile-picture-png.png?itok=MQ-iPuNG" className="home-page-photo" alt="user-profile" />
-        <div>
-          <Link to="./comment" className="comment-link">
+    tweetData.map(tweetItem => (
+      <Link to='/comment' key={tweetItem.id} className="tweet-item">
+        <div className="tweet-content-item">
+          <img onClick={()=>console.log('hey')} src="https://history.ucr.edu/sites/g/files/rcwecm1916/files/styles/form_preview/public/blank-profile-picture-png.png?itok=MQ-iPuNG" className="home-page-photo" alt="user-profile" />
+          <div>
             <div className="tweet-person">
-              <strong>Derick Alangi</strong>
-              <button><i className="material-icons">&#xe5d3;</i></button>
+              <strong className="stuff">Derick </strong>
+              <button className=" one" onClick={()=>console.log('hey')} ><i className="material-icons">&#xe5d3;</i></button>
             </div>
-            <p>{tweetText}</p>
-          </Link>
-          <div className="tweet-info">
-            <button onClick={() => setCommentModal(true)}>
-              <i className="fa fa-comment-o"></i>
-              5.1k
-            </button>
-            <button><i className="fa fa-retweet"></i>2.1k</button>
-            <button><i className="fa fa-heart-o"></i>3.1k</button>
-            <button><i className="fa fa-upload"></i></button>
+            <p>{tweetItem.tweetText}</p>
+            <div className="tweet-info">
+              <button className="stuff two" onClick={() => setCommentModal(true)}>
+                <i className="fa fa-comment-o"></i>
+                5.1k
+              </button>
+              <button><i className="fa fa-retweet"></i>2.1k</button>
+              <button><i className="fa fa-heart-o"></i>3.1k</button>
+              <button><i className="fa fa-upload"></i></button>
+            </div>
           </div>
         </div>
-      </div>
-      {commentModalDisplay && <CommentModal setCommentModal={setCommentModal}/>}
-    </div>
+        {commentModalDisplay && <CommentModal setCommentModal={setCommentModal}/>}
+      </Link>
+    ))
   )
 }
 
