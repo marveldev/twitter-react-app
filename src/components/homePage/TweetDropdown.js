ebcedf6381835a52a9a028d828f93e8647782c6b
Marvel Dev <marvellousalika48@gmail.com>
2021-03-15 15:46:50 +0100

    add tweetDropdown as props
    - add delete button click event

--- src/components/homePage/TweetDropdown.js
+++ src/components/homePage/TweetDropdown.js
@@ -1,12 +1,20 @@
-const TweetDropdown = ({ setTweetDropdown }) => {
+const TweetDropdown = ({ tweetDropdown, setTweetDropdown, deleteModal, setDeleteModal }) => {
   return (
     <>
       <div className="overlay" style={{backgroundColor: "transparent"}}
         onClick={() => setTweetDropdown(false)}
       >
       </div>
-      <div className="tweet-dropdown">
-        <button><i className="fa fa-trash-o"></i>Delete</button>
+      <div className="tweet-dropdown" style={{top: tweetDropdown.position}}>
+        <button
+          onClick={() => {
+            setDeleteModal({isActive: true, tweetId: deleteModal.tweetId});
+            setTweetDropdown(false)
+          }}
+        >
+          <i className="fa fa-trash-o"></i>
+          Delete
+        </button>
         <button><i className="material-icons">&#xe55e;</i>Pin to your profile</button>
         <button><i className="fa fa-code"></i>Embed Tweet</button>
         <button><i className="material-icons">&#xe1b8;</i>View Tweet activity</button>
